c479b9e208eb8530a157c46df88bad3035ff5151
BuildTools <unconfigured@null.spigotmc.org>
2022-07-11 21:48:43 +0300

    Fixed Bug that send as reference World names with color

--- src/main/java/me/ted2001/gamerulesmanager/Listeners/WorldSelectorListener.java
+++ src/main/java/me/ted2001/gamerulesmanager/Listeners/WorldSelectorListener.java
@@ -1,15 +1,16 @@
 package me.ted2001.gamerulesmanager.Listeners;
 
 import me.ted2001.gamerulesmanager.GUI.GUI;
-import me.ted2001.gamerulesmanager.Gamerules.GameruleGetter;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
 
 public class WorldSelectorListener implements Listener {
 
@@ -19,7 +20,9 @@ public class WorldSelectorListener implements Listener {
 
     public World WorldSelected;
     private GUI GUI = new GUI();
-
+    private ItemStack Normal = new ItemStack(Material.GRASS_BLOCK);
+    private ItemStack Nether = new ItemStack(Material.NETHERRACK);
+    private ItemStack End = new ItemStack(Material.END_STONE);
     @EventHandler
     public void onGuiClick(InventoryClickEvent e) {
         try {
@@ -27,12 +30,15 @@ public class WorldSelectorListener implements Listener {
                 if (e.getCurrentItem() == null)
                     return;
                 e.setCancelled(true);
-                Inventory gui = e.getClickedInventory();
                 Player p = (Player) e.getWhoClicked();
-                String world_name = e.getCursor().toString();
-                World world = Bukkit.getWorld(world_name);
-                WorldSelected = world;
-                p.openInventory(GUI.gameruleSetterGui(p,world));
+                String world_name;
+                String clickitem = e.getCurrentItem().getType().toString();
+                if(clickitem.equalsIgnoreCase("GRASS_BLOCK") || clickitem.equalsIgnoreCase("NETHERRACK") || clickitem.equalsIgnoreCase("END_STONE")) {
+                    world_name = e.getCurrentItem().getItemMeta().getDisplayName();
+                    World world = Bukkit.getWorld(world_name);
+                    WorldSelected = world;
+                    p.openInventory(GUI.gameruleSetterGui(p,world));
+                }
 
             }
         } catch (NullPointerException ex) {}
